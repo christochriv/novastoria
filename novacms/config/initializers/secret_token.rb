7508d8b60df4f4c06d796f6ae83ee510be99008f
Chris <c.chriv@gmail.com>
2014-10-22 17:15:02 +0200

    Installation de RefineryCMS

---
+++ novacms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Novacms::Application.config.secret_token = '84f62533c104d248e5b45c078ea456d9c64c68bce9a41341c9f7c8c4b5caa51f1c88b3b3f9c4cd9c68b9780d9f539bd15bc6a4049d068e5bee3421c9bd125977'
